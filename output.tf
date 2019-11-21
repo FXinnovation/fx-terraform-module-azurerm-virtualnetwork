31bdf4da9d328d58f1eb4c93f4e34dcd240e2b27
Maxime Roy <maxime.roy@fxinnovation.com>
2019-11-21 14:38:03 -0500

    Added output to the module

---
+++ output.tf
@@ -0,0 +1,24 @@
+output "virtual_network_id" {
+  description = "Virtual network generated id"
+  value       = azurerm_virtual_network.this.id
+}
+
+output "virtual_network_location" {
+  description = "Virtual network location"
+  value       = azurerm_virtual_network.this.location
+}
+
+output "virtual_network_name" {
+  description = "Virtual network name"
+  value       = azurerm_virtual_network.this.name
+}
+
+output "virtual_network_address_space" {
+  description = "Virtual network space"
+  value       = azurerm_virtual_network.this.address_space
+}
+
+output "virtual_network_dns_servers" {
+  description = "Virtual network DNS Servers"
+  value       = azurerm_virtual_network.this.dns_servers
+}
