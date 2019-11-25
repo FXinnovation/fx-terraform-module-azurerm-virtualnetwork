e4ee77ac0804462499b6976d8484675eec64b021
Maxime Roy <maxime.roy@fxinnovation.com>
2019-11-25 17:32:37 -0500

    Removed interpolation

--- examples/default/provider.tf
+++ examples/default/provider.tf
@@ -5,9 +5,9 @@ terraform {
 
 #Set the Provider
 provider "azurerm" {
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  subscription_id = "${var.subscription_id}"
-  tenant_id       = "${var.tenant_id}"
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
   version         = ">= 1.36.0" #1.36.0 to support the resource azurerm_bastion_host #1.37.0 fix a bug with the bastion host naming #With "=1.32.0" No warning with version the nsg and route linkd
 }
