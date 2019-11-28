d662789258cf1eab6d9aa5d60658854bb15b9a32
Maxime Roy <maxime.roy@fxinnovation.com>
2019-11-28 09:33:36 -0500

    Changed output

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,65 @@
 output "virtual_network_id" {
   description = "Virtual network generated id"
-  value       = azurerm_virtual_network.this[count.index].id
+  value = element(
+    concat(
+      azurerm_virtual_network.this.*.id,
+      list("")
+    ),
+    0
+  )
 }
 
 output "virtual_network_location" {
   description = "Virtual network location"
-  value       = azurerm_virtual_network.this[count.index].location
+  value = element(
+    concat(
+      azurerm_virtual_network.this.*.location,
+      list("")
+    ),
+    0
+  )
 }
 
 output "virtual_network_name" {
   description = "Virtual network name"
-  value       = azurerm_virtual_network.this[count.index].name
+  value = element(
+    concat(
+      azurerm_virtual_network.this.*.name,
+      list("")
+    ),
+    0
+  )
 }
 
 output "virtual_network_resource_group_name" {
   description = "Virtual network resource group name"
-  value       = azurerm_virtual_network.this[count.index].resource_group_name
+  value = element(
+    concat(
+      azurerm_virtual_network.this.*.resource_group_name,
+      list("")
+    ),
+    0
+  )
 }
 
 output "virtual_network_address_space" {
   description = "Virtual network space"
-  value       = azurerm_virtual_network.this[count.index].address_space
+  value = element(
+    concat(
+      azurerm_virtual_network.this.*.address_space,
+      list("")
+    ),
+    0
+  )
 }
 
 output "virtual_network_dns_servers" {
   description = "Virtual network DNS Servers"
-  value       = azurerm_virtual_network.this[count.index].dns_servers
+  value = element(
+    concat(
+      azurerm_virtual_network.this.*.dns_servers,
+      list("")
+    ),
+    0
+  )
 }
