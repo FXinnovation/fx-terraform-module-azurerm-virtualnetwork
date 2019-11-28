efc365d4696baa22a09dd5d28d5164bf019b4b30
Maxime Roy <maxime.roy@fxinnovation.com>
2019-11-28 09:24:27 -0500

    trying other solution...

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "virtual_network_id" {
   description = "Virtual network generated id"
-  value       = azurerm_virtual_network.this.*.id
+  value       = azurerm_virtual_network.this[count.index].id
 }
 
 output "virtual_network_location" {
   description = "Virtual network location"
-  value       = azurerm_virtual_network.this.*.location
+  value       = azurerm_virtual_network.this[count.index].location
 }
 
 output "virtual_network_name" {
   description = "Virtual network name"
-  value       = azurerm_virtual_network.this.*.name
+  value       = azurerm_virtual_network.this[count.index].name
 }
 
 output "virtual_network_resource_group_name" {
   description = "Virtual network resource group name"
-  value       = azurerm_virtual_network.this.*.resource_group_name
+  value       = azurerm_virtual_network.this[count.index].resource_group_name
 }
 
 output "virtual_network_address_space" {
   description = "Virtual network space"
-  value       = azurerm_virtual_network.this.*.address_space
+  value       = azurerm_virtual_network.this[count.index].address_space
 }
 
 output "virtual_network_dns_servers" {
   description = "Virtual network DNS Servers"
-  value       = azurerm_virtual_network.this.*.dns_servers
+  value       = azurerm_virtual_network.this[count.index].dns_servers
 }
